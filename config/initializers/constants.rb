0a113f4f0966a5ab5321ed0d626c751134ed21a0
bill-auger <mr.j.spam.me@gmail.com>
2016-06-26 11:18:42 -0400

    implement navbar

---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+NAV_MENU_ID = 'nav-menu-span'
+
+LCTV_URL           = 'https://www.livecoding.tv/'
+# LCTV_BADGES_URL    = 'https://badges.livecoding.tv/' # TODO:
+LCTV_BADGES_URL    = '/badges' # TODO:
+LCTV_EMOTICONS_URL = '/emoticons'
+LCTV_PROJECTS_URL  = '/projects'
+AV_URL = 'http://afterdesign.ru/tools/static/img/av.jpg' # FIXME:
