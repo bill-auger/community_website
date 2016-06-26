b90301bb89b51bdaf670b73d7b0baa475c463c2b
bill-auger <mr.j.spam.me@gmail.com>
2016-06-26 17:31:18 -0400

    implement dummy home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Lctv::Application.routes.draw do
+  resources :projects
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-#  root 'home#index'
   root :controller => "home" , :action => "home"
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
