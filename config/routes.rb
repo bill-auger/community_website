1c53a1acde5c1d343a7be19b3120228263d0f195
bill-auger <mr.j.spam.me@gmail.com>
2016-06-26 18:48:59 -0400

    add projects scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Lctv::Application.routes.draw do
   resources :projects
   resources :users
 
+  root :controller => "home" , :action => "home"
+  get  'badges'    => 'home#badges'
+  get  'emoticons' => 'home#emoticons'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :controller => "home" , :action => "home"
+  # root 'home#index'
 
   # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619095945) do
+ActiveRecord::Schema.define(version: 20160626214750) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.string   "repo"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "nick"
